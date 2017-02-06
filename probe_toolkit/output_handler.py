211f3d8606a3ec0e193421de9b6211b1d3dcc9f7
CYBERDEVILNL <unknown@unknown>
2017-02-05 20:59:41 -0600

    - Now it doens't refresh whole screen on new data but erases the lines and replaces them.  - Added time_ago_format option in the config, if set True it will replace the datetime and show (examples:) "1 min 21 sec ago." instead of "2017-01-01 00:00:00"

--- probe_toolkit/output_handler.py
+++ probe_toolkit/output_handler.py
@@ -1,6 +1,10 @@
 import datetime
+import time
+from time import mktime
+import sys
 class output_handler(object):
 	def __init__(self,config):
+		self.config = config
 		self.height = 0
 		self.width = 0
 		self.header = """probe_toolkit - probecap\n"""
@@ -27,15 +31,17 @@ class output_handler(object):
 					'out_notice'	: 1,
 					'out_debug'		: 1
 				}
-		self.disable_color = 0
-		if config['disable_color']:
+		self.clear_screen()
+		self.disable_color = True
+		if 'disable_color' in config:
 			self.disable_color = config['disable_color']
-		self.set_dimensions(config)
+		self.set_dimensions()
 		self.line_buffer = [""]*self.height
 		self.set_vars(self.out,config)
 		self.set_vars(self.log,config)
-		print(chr(27) + "[2J") # clear screen
-
+		self.time_ago_format = False
+		if 'time_ago_format' in config:
+			self.time_ago_format = config['time_ago_format']
 		if (config['log_info']
 			or config['log_debug'] 
 			or config['log_warn']
@@ -45,7 +51,6 @@ class output_handler(object):
 				self.log_file = open(config['log_file'], 'a')
 			except:
 				self.output("ERROR","Could not open log_file")
-
 		self.print_vars(config)
 
 	def __enter__(self):
@@ -54,23 +59,30 @@ class output_handler(object):
 	def __exit__(self, exc_type, exc_value, traceback):
 		self.log_file.close()
 
+	def clear_screen(self):
+		# use sys.stdout.write so there is no new line.
+		# first clear screen then set cur pos top left
+		sys.stdout.write(chr(27) + "[2J" + chr(27) + "[0;0H")
+
 	# Need to do this after something changed, example the header size
-	def set_dimensions(self,config=None):
-		height_offet = 2
-		if not config['height'] or not config['width']:
+	# This will reset the line buffer
+	def set_dimensions(self):
+		height_offet = 1
+		if not self.config['height'] or not self.config['width']:
 			import os
 			term_rows, term_columns = os.popen('stty size', 'r').read().split() # https://stackoverflow.com/questions/566746/how-to-get-console-window-width-in-python
-			if not config['height']:
+			if not self.config['height']:
 				self.height = int(term_rows) - (len(self.header.split('\n'))+height_offet)
 			else:
-				self.height = int(config['height']) - (len(self.header.split('\n'))+height_offet)
-			if not config['width']:
+				self.height = int(self.config['height']) - (len(self.header.split('\n'))+height_offet)
+			if not self.config['width']:
 				self.width = int(term_columns)
 			else:
-				self.width = int(config['width'])
+				self.width = int(self.config['width'])
 		else: # if both are set in the config (and both are not 0)
-			self.height = int(config['height']) - (len(self.header.split('\n'))+height_offet)
-			self.width = int(config['width'])
+			self.height = int(self.config['height']) - (len(self.header.split('\n'))+height_offet)
+			self.width = int(self.config['width'])
+		self.line_buffer = [""]*self.height
 
 	def set_vars(self,var,config):
 		for k in var:
@@ -100,11 +112,20 @@ class output_handler(object):
 		else:
 			self.line_buffer = [line] + self.line_buffer
 
+	def print_header(self):
+		sys.stdout.write(chr(27) + "[0;0H")	# Go to top left
+		for x in self.header.split('\n'):
+			print(chr(27) + "[K " + x) 		# erase line + add new
+		sys.stdout.write(chr(27) + "[s") 	# save cur pos
+
 	def print_buffer(self):
-		print(chr(27) + "[2J") #http://bluesock.org/~willg/dev/ansi.html
-		print(self.header)	
+		self.print_header()
+		sys.stdout.write(chr(27) + "[u") 	# restore cur pos
+
 		for l in self.line_buffer:
-			print(l)
+			if self.time_ago_format:		# if time_ago_format is set True in config
+				l = self.redraw_time_ago(l)	# return time ago string
+			print(chr(27) + "[K" + l) 		# erase line + add new
 
 	def add_coloring(self,msg,colors):
 		msg = chr(27) + colors + msg;
@@ -115,18 +136,49 @@ class output_handler(object):
 		for x in range(1,(int(self.width) - len(msg.expandtabs()))):
 			msg += " "
 		return msg
+
+	def redraw_time_ago(self,line):
+		if len(line) > 25:
+			if not self.disable_color: offset = 10
+			else: offset = 2
+			_datetime = line[offset:19+offset]
+			t_ago = self.time_ago(_datetime)
+			line = line[0:offset] + t_ago + line[offset+19:]
+		return line
+
+	def time_ago(self,_datetime):
+		time_ago_str = ""
+		if _datetime != '':
+			m_datetime = datetime.datetime.strptime(_datetime, '%Y-%m-%d %H:%M:%S')
+			_time_ago = datetime.datetime.now() - m_datetime
+			hours = _time_ago.seconds / 3600
+			if hours > 0:
+				return _datetime
+			seconds = _time_ago.seconds
+			if seconds < 2:
+				return "     just now.".ljust(19)
+			minutes = _time_ago.seconds / 60
+			if minutes > 0:
+				time_ago_str += "%02d min "%minutes
+			if seconds - (minutes * 60) > 0:
+				time_ago_str += "%02d sec "%(seconds - (minutes * 60))
+			time_ago_str += "ago."
+			return time_ago_str.rjust(19)
+
 		
-	def output(self,msg_level,msg,msg_datetime=str(datetime.datetime.now())[:19]):
+	def output(self,msg_level,msg,msg_datetime=None):
 		switch = { 'ERROR'	: (self.log['log_error'],self.out['out_error'],self.color['RED_BLACK']),
 					'INFO'	: (self.log['log_info'],self.out['out_info'],self.color['GREEN_WHITE']),
 					'NOTICE': (self.log['log_notice'],self.out['out_notice'],self.color['BLUE_WHITE']),
 					'DEBUG'	: (self.log['log_debug'],self.out['out_debug'],self.color['YELLOW_BLACK']),
 					'WARN'	: (self.log['log_warn'],self.out['out_warn'],self.color['PURPLE_HAZE']),
 		}
+		if not msg_datetime:
+			msg_datetime = str(datetime.datetime.now())[:19]
 		if switch[msg_level][0]:
 			self.log_file.write(msg_datetime + "," + msg_level + "," + msg + "\n");
 		if switch[msg_level][1]:
-			msg = "[" + msg_datetime + " " + msg_level.ljust(6) + "] " + msg
+			msg = "[ " + msg_datetime + " " + msg_level.rjust(6) + " ] " + msg
 			msg = self.fill_str_size(msg)
 			if not self.disable_color:
 				msg = self.add_coloring(msg,switch[msg_level][2])
